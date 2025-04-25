9dd55a83519f9ce31c26b54b135a798388733475
rukundowilson <rukundowilson5@gmail.com>
2025-04-25 12:09:21 +0200

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,65 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers=generate(1,10,10000);
-        mergesort(numbers,0,99);
+        int[] numbers = generate(1, 99, 1000);
+        mergeSort(numbers, 0, numbers.length - 1);
         display(numbers);
-        System.out.println(generate(0,10,10));
-
+        System.out.println();
+        System.out.println(Arrays.toString(generate(0, 10, 10)));
     }
-   /*Generate 10000 random integers between 1 and 100
-    */
+
     public static int[] generate(int low, int high, int size) {
         Random rand = new Random();
-        int[] result = new int[size];
+        int[] resultStore = new int[size];
         for (int i = 0; i < size; i++) {
-            result[i] = rand.nextInt(high - low + 1) + low;
+            resultStore[i] = rand.nextInt(high - low + 1) + low;
         }
-        return result;
+        return resultStore;
     }
-  public static void mergesort(int[] a,int low, int high ){
 
-  }
-  public static void display(int[]a){
-     for(int x: a){
-         System.out.print(x );
-         System.out.print("\t");
-     }
-  }
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+    // l have created a new merge function for more readability and mantain clean code
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        System.arraycopy(arr, left, leftArray, 0, n1);
+        System.arraycopy(arr, mid + 1, rightArray, 0, n2);
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                arr[k++] = leftArray[i++];
+            } else {
+                arr[k++] = rightArray[j++];
+            }
+        }
+
+        while (i < n1) {
+            arr[k++] = leftArray[i++];
+        }
+
+        while (j < n2) {
+            arr[k++] = rightArray[j++];
+        }
+    }
+
+    public static void display(int[] arr) {
+        for (int x : arr) {
+            System.out.print(x + "\t");
+        }
+    }
 }
